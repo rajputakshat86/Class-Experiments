72887788c4234dbbaf4e2cde9132d886a91784b9
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-15 11:58:40 +0530

    Add files via upload

---
+++ static-library.c
@@ -0,0 +1,48 @@
+
+#ifndef ARITHMETIC_H
+#define ARITHMETIC_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int divide(int a, int b);
+
+#endif
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+int mul(int a, int b)
+{
+    return a * b;
+}
+
+int divide(int a, int b)
+{
+    if (b == 0)
+    {
+        return 0;
+    }
+    return a / b;
+}
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 20, y = 5;
+
+    printf("Addition = %d\n", add(x, y));
+    printf("Subtraction = %d\n", sub(x, y));
+    printf("Multiplication = %d\n", mul(x, y));
+    printf("Division = %d\n", divide(x, y));
+
+    return 0;
+}
